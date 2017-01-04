a0c1105eceafbe3181c6001552616d2763cf08ce
robflop <robflop98@web.de>
2017-01-04 13:27:36 +0100

    Add command files

---
+++ commands/otherEvents.js
@@ -0,0 +1,20 @@
+const config = require('../config.json'); // import configuration
+const fs = require('fs'); // for log writing
+const moment = require('moment'); // part of log writing
+
+module.exports = {
+	"ready": function ready(bot) {
+	console.log(`${bot.user.username} ready!`); // console log a ready message
+	bot.user.setGame("try 'robbot, help' !"); // set default game status
+	},
+	"join": function join(bot) {
+	console.log(`${bot.user.username} has joined a new server! ("${guild.name}")`);
+	fs.appendFileSync(`${config.logPath}${config.serverLog}`, `\n[${moment().format('DD/MM/YYYY HH:mm:ss')}][SERVERS] ${bot.user.username} has joined the '${guild.name}' server!`); 
+	// Log which server was joined and when
+	},
+	"leave": function leave(bot) {
+	console.log(`${bot.user.username} has left a server! ("${guild.name}")`);
+	fs.appendFileSync(`${config.logPath}${config.serverLog}`, `\n[${moment().format('DD/MM/YYYY HH:mm:ss')}][SERVERS] ${bot.user.username} has left the '${guild.name}' server!`); 
+	// Log which server was left and when
+	}
+};
